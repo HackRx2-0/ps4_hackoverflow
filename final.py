96ff2cbc52588da1dcfa2ad5deb081e702d874d7
sidx255 <64124175+sidx255@users.noreply.github.com>
2021-07-24 10:10:09 +0530

    Update final.py

--- final.py
+++ final.py
@@ -1,80 +1,116 @@
+import sys
+import json
 import pandas as pd
 from nltk.tokenize import RegexpTokenizer
 from nltk.corpus import stopwords, wordnet
 from nltk.stem.porter import PorterStemmer
 
-def empty_set(some_set):
-    return some_set == set()
+def read_in():
+    lines = sys.stdin.readlines()
+    # Since our input would only be having one line, parse our JSON data from that
+    return json.loads(lines[0])
 
-tokenizer = RegexpTokenizer(r'\w+')
-p_stemmer = PorterStemmer()
+def main():
 
-df = pd.read_csv ('internal.csv')
-ls = df['Word'].tolist()
+    def empty_set(some_set):
+        return some_set == set()
 
-words = []
+    def results():
+        res = set(i for i in ls if subs in i)
+        if empty_set(res) == False: print (str(res))
 
-# ENTER QUERY HERE
-subs = 'apply for credit card'
-#res1 = list(i for i in ls if subs in i)
-FINAL = []
-FINAL.append([i for i in ls if subs in i])
-#if empty_set(res1) == False: print (str(res1))
 
-raw = subs.lower()
-tokens = tokenizer.tokenize(raw)
-stop_words = set(stopwords.words('english'))
-stopped_tokens = [subs for subs in tokens if not subs in stop_words]
+    tokenizer = RegexpTokenizer(r'\w+')
 
-words.append(stopped_tokens)
+    df = pd.read_csv ('internal.csv')
+    ls = df['Word'].tolist()
 
-#words = subs.split()
-for each in words:
-    for subs in each:
-        #res = set(i for i in ls if subs in i)
-        #if empty_set(res) == False: 
-        FINAL.append([i for i in ls if subs in i])
-            #print (str(res), end=',')
-        i=1
-        if i==0:
-            synonyms = []
-            for syn in wordnet.synsets(subs):
-                #print(syn)
-                for l in syn.lemmas():
-                    synonyms.append(l.name())
-            
-            final = list(dict.fromkeys(synonyms))
-            for new in final:
-                #res = set(i for i in ls if subs in i)
-                FINAL.append([i for i in ls if subs in i])
-            #print(final)
+    words = []
 
-recall = 0.0
-count_rev=0
-count_tot=0
-for i in ls:
+    # ENTER QUERY HERE
+    subs = read_in()
+    FINAL = []
+    FINAL.append([i for i in ls if subs in i])
+    #if empty_set(res1) == False: print (str(res1))
+
+    raw = subs.lower()
+    tokens = tokenizer.tokenize(raw)
+    stop_words = set(stopwords.words('english'))
+    stopped_tokens = [subs for subs in tokens if not subs in stop_words]
+    words.append(stopped_tokens)
+
+    #words = subs.split()
+    for each in words:
+        for subs in each:
+            #res = set(i for i in ls if subs in i)
+            #if empty_set(res) == False: 
+            FINAL.append([i for i in ls if subs in i])
+                #print (str(res), end=',')
+            i=1
+            if i==0:
+                synonyms = []
+                for syn in wordnet.synsets(subs):
+                    #print(syn)
+                    for l in syn.lemmas():
+                        synonyms.append(l.name())
+                
+                final = list(dict.fromkeys(synonyms))
+                for new in final:
+                    #res = set(i for i in ls if subs in i)
+                    FINAL.append([i for i in ls if subs in i])
+                #print(final)
+
+    new = []
+    i = 0
+    for string in FINAL:
+        if(i<=100):
+            if len(string) != 0:
+                new.append(string)
+            i=i+1
+        else: break
+
+    # PRECISION and RECALL
+
+    recall = 0.0
+    count_rev=0
+    count_tot=0
+    for i in ls:
+            if subs in i:
+                count_rev = count_rev+1
+    for i in FINAL[1]:
         if subs in i:
-            count_rev = count_rev+1
-for i in FINAL[1]:
-    if subs in i:
-        count_tot=count_tot+1
-recall = count_tot/count_rev
-print(recall)
+            count_tot=count_tot+1
+    recall = count_tot/count_rev
 
-precision = 0.0
-count_rev=0
-count_tot=0
-for j in range(2):
-    for i in FINAL[j]:
-        if subs in i:
-            count_rev = count_rev+1
+    precision = 0.0
+    count_rev=0
+    count_tot=0
+    for j in range(2):
+        for i in FINAL[j]:
+            if subs in i:
+                count_rev = count_rev+1
 
-for i in ls:
-        if subs in i:
-            count_tot = count_tot+1
+    for i in ls:
+            if subs in i:
+                count_tot = count_tot+1
 
-#if duplicates       
-if count_rev>count_tot: count_rev=count_tot
+    #if duplicates       
+    if count_rev>count_tot: count_rev=count_tot
 
-precision = count_rev/count_tot
-print(precision)
+    precision = count_rev/count_tot
+
+    new = [[precision] + [recall]] + new
+
+    for string in new:
+        if len(string) != 0:
+            print(string, end=',')
+
+
+    '''
+    lines = read_in()
+    n = ["Holiday package", "Vacation"]
+    for i in n:
+        print(i,end=',');
+'''
+if __name__ == '__main__':
+    main()
